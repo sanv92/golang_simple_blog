75534bf3dac2eb4b003a2e23e781b198adf5d29a
doctype.studio <doctypestudio@gmail.com>
2017-06-11 22:11:14 +0300

    fix

--- main.go
+++ main.go
@@ -16,42 +16,32 @@ var (
 var tpl = template.Must(template.ParseGlob("templates/*"))
 
 
-type View string
-
-func (v View) Render(data interface{}, w http.ResponseWriter, r *http.Request) error {
-	if err := tpl.ExecuteTemplate(w, string(v) + ".tmpl", data); err != nil {
-		return ErrTemplateDoesNotExist
-	}
-
-	return nil
-}
-
-
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	//var menu []json.Menu
 	//ReadJSON(&menu)
 
-	View("home").Render("1111", w, r)
+	tpl.ExecuteTemplate(w, "home.tmpl", "11122")
 }
 
 func AboutPage(w http.ResponseWriter, r *http.Request) {
-	View("about").Render(nil, w, r)
+	tpl.ExecuteTemplate(w, "about.tmpl", nil)
 }
 
 func ContactsPage(w http.ResponseWriter, r *http.Request) {
-	View("contacts").Render(nil, w, r)
+	tpl.ExecuteTemplate(w, "contacts.tmpl", nil)
 }
 
 func NewsList(w http.ResponseWriter, r *http.Request) {
-	View("news_list").Render("2222", w, r)
+	tpl.ExecuteTemplate(w, "news_list.tmpl", "2222")
 }
 
 func NewsFull(w http.ResponseWriter, r *http.Request) {
 	//newsID := strings.SplitN(req.URL.Path, "/", 4)[3]
 	//fmt.Fprint(w, string(newsID))
 
-	View("news_full").Render("4444", w, r)
+	tpl.ExecuteTemplate(w, "news_full.tmpl", "44444")
 }
+
 
 const (
 	NewsFile = "pkg/config/news.json"
@@ -80,17 +70,8 @@ func ReadJSON(result interface{}) error {
 }
 
 
-func middlewareMenu(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//var menu []json.Menu
-		//ReadJSON(&menu)
-
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
-	http.HandleFunc("/", middlewareMenu(HomePage))
+	http.HandleFunc("/", HomePage)
 	http.HandleFunc("/about", AboutPage)
 	http.HandleFunc("/contacts", ContactsPage)
 	http.HandleFunc("/news/", NewsList)
